15d703405eba94e0bc1984e2b00a1100d675734d
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-09-21 14:27:55 -0300

    Repository Product e Stock com @Repository.

--- src/main/java/com/productcenter/ProductCenter/repository/ProductRepository.java
+++ src/main/java/com/productcenter/ProductCenter/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.productcenter.ProductCenter.repository;
 
 import com.productcenter.ProductCenter.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/com/productcenter/ProductCenter/repository/StockRepository.java
+++ src/main/java/com/productcenter/ProductCenter/repository/StockRepository.java
@@ -2,6 +2,8 @@ package com.productcenter.ProductCenter.repository;
 
 import com.productcenter.ProductCenter.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 }
