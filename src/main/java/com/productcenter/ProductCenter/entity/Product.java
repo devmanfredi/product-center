5423b1fd095a4b32ac6b90972e6d9ebe7e304376
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-09-21 14:13:40 -0300

    Classe Product criada no banco.

--- src/main/java/com/productcenter/ProductCenter/entity/Product.java
+++ src/main/java/com/productcenter/ProductCenter/entity/Product.java
@@ -4,8 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -16,10 +22,20 @@ import java.time.LocalDateTime;
 @Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private BigDecimal value;
+
+    @CreatedDate
     private LocalDateTime createAt;
+
+    @LastModifiedDate
     private LocalDateTime updateAt;
 
 }
